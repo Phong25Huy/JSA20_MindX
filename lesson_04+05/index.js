2aa449167897f03909ba3bf768d11a19be1c0b6e
Phong25Huy <phongnguyenhuy63@gmail.com>
2024-08-03 15:42:41 +0700

    end lesson 4 + 5

--- lesson_04+05/index.js
+++ lesson_04+05/index.js
@@ -1,14 +1,38 @@
-var to_do_list = [];
+var todoList = [];
 
-console.log(to_do_list);
+console.log(todoList);
 
 var todoForm = document = document.getElementById("todo-form")
 
 var handleSumbitForm = function (event) {
-    event.preventDefault();
+    event.preventDefault()  
 
     var todoInput = document.getElementById("todo-input")
-    console.log(todoInput.value)
-}
+    var todoValue =todoInput.value
 
+    var todoNew = {
+        id: Date.now(),
+        content: todoValue,
+        completed: false,
+    }
+    todoList.push(todoNew)
+
+    var todoListElement = document.getElementById("todo-list")
+    var todoItemElement = `
+        <li class="d-flex justify-content-between list-group-item">
+            <span class="task-text">${todoValue}</span>
+            <input
+                type="text"
+                class="form-control edit-input"
+                style="display: none"
+                value="Lear HTML"
+            />
+            <div class="btn-group">
+                <button class="btn btn-danger btn-sm delete-btn">✕</button>
+                <button class="btn btn-primary btn-sm edit-btn">✎</button>
+            </div>
+        </li>                        
+    `
+    todoListElement.insertAdjacentHTML("afterbegin",todoItemElement)
+}
 todoForm.addEventListener("submit", handleSumbitForm);
