416b6323c606d6bdf8526cb80642424f0c4a1ddf
Phong25Huy <phongnguyenhuy63@gmail.com>
2024-08-03 14:48:28 +0700

    Update index.js

--- lesson_04+05/index.js
+++ lesson_04+05/index.js
@@ -1,3 +1,14 @@
 var to_do_list = [];
 
-console_log(to_do_list)
+console.log(to_do_list);
+
+var todoForm = document = document.getElementById("todo-form")
+
+var handleSumbitForm = function (event) {
+    event.preventDefault();
+
+    var todoInput = document.getElementById("todo-input")
+    console.log(todoInput.value)
+}
+
+todoForm.addEventListener("submit", handleSumbitForm);
