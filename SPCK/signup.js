aaba3f62fe571669da33e186f3f6bf22b1896bf4
Phong25Huy <phongnguyenhuy63@gmail.com>
2024-10-01 21:14:48 +0700

    Change signup_signin_localdata

--- SPCK/signup.js
+++ SPCK/signup.js
@@ -1,12 +1,19 @@
 
-
+function initDatabase(){
+    const users = JSON.parse(localStorage.getItem("users"))
+    if (users ?.length === 0){
+        localStorage.setItem("users",JSON.stringify([]));
+    }
+}
 document.getElementById("form-signup").addEventListener("submit", function(e){
     e.preventDefault();
-    var input_phone = document.getElementById("tel").value
+    initDatabase()
+    var input_phone = document.getElementById("tel")
     var input_email_signup = document.getElementById("email_signup").value
     var input_password = document.getElementById("password_signup").value
     var input_cf_password = document.getElementById("cf_password").value
     if (input_phone.length == 10 && input_email_signup.length > 0 && input_password.length > 7 && input_cf_password == input_password){
+        console.log("đã nhảy vào hàm if1")
         var count = 0
         
         for (var i in account_list){
@@ -28,20 +35,26 @@ document.getElementById("form-signup").addEventListener("submit", function(e){
                 continue
             }
         }
+        console.log(count)
         if (count == account_list.length){
+            console.log("Signup thành công")
             var accountNew = {
                 phone:input_phone,
                 email:input_email_signup,
                 password:input_password
             }
-            console.log(accountNew)
-            console.log(account_list)
-            account_list.push(accountNew)
+            
             document.getElementById("tel").classList.remove("is-invalid")
             document.getElementById("tel-feedback").innerHTML = ""
             document.getElementById("email_signup").classList.remove("is-invalid")
             document.getElementById("email-feedback_signup").innerHTML = ""
+            const users = JSON.parse(localStorage.getItem("users"))
+
+            users.push(accountNew)
+
+            localStorage.setItem("users",JSON.stringify([]));
             
+            console.log(users)
             // window.location.href = "signin.html"
         }
         
@@ -53,6 +66,7 @@ document.getElementById("form-signup").addEventListener("submit", function(e){
         }
     }
     else{
+        console.log("đã nhảy vào hàm else1")
         if (input_phone == ""){
             document.getElementById("tel").classList.add("is-invalid")
             document.getElementById("tel-feedback").innerHTML = "Không được để trống số điện thoại"
@@ -98,3 +112,5 @@ document.getElementById("form-signup").addEventListener("submit", function(e){
         }
     }
 })
+
+
