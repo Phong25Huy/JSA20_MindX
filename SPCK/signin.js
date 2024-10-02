a4250a49155f2ac7e1d46c7f9260f0152665789a
Phong25Huy <phongnguyenhuy63@gmail.com>
2024-10-02 21:54:42 +0700

    Change signin_signup method

--- SPCK/signin.js
+++ SPCK/signin.js
@@ -2,59 +2,24 @@
 
 document.getElementById("form-signin").addEventListener("submit", function (e){
     e.preventDefault();
-    var input_email = document.getElementById("email").value
-    var input_password = document.getElementById("password").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
     
-    if (input_email == ""){
-        document.getElementById("email").classList.add("is-invalid")
-        document.getElementById("email-feedback").innerHTML="Không được để trống email"
-    }
-    else{
-        document.getElementById("email").classList.remove("is-invalid")
-        document.getElementById("email-feedback").innerHTML = ""
-    }
-    if (input_password == ""){
-        document.getElementById("password").classList.add("is-invalid")
-        document.getElementById("pass-feedback").innerHTML="Không được để trống password"
-    }
-    else{
-        document.getElementById("password").classList.remove("is-invalid")
-        document.getElementById("pass-feedback").innerHTML = ""
-    }
-
-  
-    console.log(account_list)
-    var count_s = 0
-    for (var CheckAccount in account_list){
-        console.log(account_list[CheckAccount].email)
-        if(account_list[CheckAccount].email === input_email) {
-            console.log("Email hợp lệ")
-            if(account_list[CheckAccount].password == input_password) {
-                console.log("dang nhap thanh cong")
-                document.getElementById("email").classList.remove("is-invalid")
-                document.getElementById("password").classList.remove("is-invalid")
-                document.getElementById("pass-feedback").innerHTML=""
-                document.getElementById("email-feedback").innerHTML=""
-                // chyuyen user ve trang home
-                
-                window.location.href = "index.html"
-                break
-            } else {
-                document.getElementById("password").classList.add("is-invalid")
-                document.getElementById("pass-feedback").innerHTML="Sai password"
-                break
-            }
+    
+    const users = JSON.parse(localStorage.getItem("users"));
+    console.log(users)
+    var Exituser =  users.find((user) => user.email == email)
+    if (Exituser){
+        if (Exituser.password == password){
+            window.location.href = "index.html"
         }
         else{
-            console.log("count_s:",count_s)
-            count_s = count_s + 1
-            continue
-        } 
-        
+            document.getElementById("password").classList.add("is-invalid")
+            document.getElementById("pass-feedback").innerHTML="Sai password"
+        }
     }
-    if(count_s == account_list.length) {
+    else{
         document.getElementById("email").classList.add("is-invalid")
         document.getElementById("email-feedback").innerHTML="Email chưa được đăng kí"
-        console.log(account_list)   
-    }   
+    }
 })
--- SPCK/signup.js
+++ SPCK/signup.js
@@ -1,6 +1,6 @@
 
 function initDatabase(){
-    const users = JSON.parse(localStorage.getItem("users"))
+    const users = JSON.parse(localStorage.getItem("users")) || [];
     if (users ?.length === 0){
         localStorage.setItem("users",JSON.stringify([]));
     }
@@ -8,109 +8,57 @@ function initDatabase(){
 document.getElementById("form-signup").addEventListener("submit", function(e){
     e.preventDefault();
     initDatabase()
-    var input_phone = document.getElementById("tel")
-    var input_email_signup = document.getElementById("email_signup").value
-    var input_password = document.getElementById("password_signup").value
-    var input_cf_password = document.getElementById("cf_password").value
-    if (input_phone.length == 10 && input_email_signup.length > 0 && input_password.length > 7 && input_cf_password == input_password){
-        console.log("đã nhảy vào hàm if1")
-        var count = 0
-        
-        for (var i in account_list){
-        
-            if (account_list[i].phone == input_phone){
-                break
-                
-            }
-            if (account_list[i].email == input_email_signup){
-                
-                break
-            }
-            else if(account_list[i].phone != input_phone){
-                count = count + 1
-                continue
-            }
-            else if(account_list[i].email != input_email_signup){
-                count = count + 1
-                continue
-            }
+    var phone = document.getElementById("tel").value
+    var email = document.getElementById("email_signup").value
+    var password = document.getElementById("password_signup").value
+    var cf_password = document.getElementById("cf_password").value
+    var user = {
+        phone,
+        email,
+        password,
+    }
+    
+    const users = JSON.parse(localStorage.getItem("users"));
+
+    var Exituseremail =  users.find((user) => user.email === email)
+    var Exituserphone = users.find((user) => user.phone === phone)
+
+    if (phone.length == 10 && email.length > 0 && password.length >= 8 && password == cf_password){
+        if (Exituseremail && Exituserphone){
+            document.getElementById("email_signup").classList.add("is-invalid")
+            document.getElementById("email-feedback_signup").innerHTML="Email đã được đăng kí"
+            document.getElementById("tel").classList.add("is-invalid")
+            document.getElementById("tel-feedback").innerHTML="Số điện thoại đã được đăng kí"
         }
-        console.log(count)
-        if (count == account_list.length){
-            console.log("Signup thành công")
-            var accountNew = {
-                phone:input_phone,
-                email:input_email_signup,
-                password:input_password
-            }
-            
+        else if (Exituseremail){
+            document.getElementById("email_signup").classList.add("is-invalid")
+            document.getElementById("email-feedback_signup").innerHTML="Email đã được đăng kí"
             document.getElementById("tel").classList.remove("is-invalid")
-            document.getElementById("tel-feedback").innerHTML = ""
+            document.getElementById("tel-feedback").innerHTML=""
+        }
+        else if (Exituserphone){
+            document.getElementById("tel").classList.add("is-invalid")
+            document.getElementById("tel-feedback").innerHTML="Số điện thoại đã được đăng kí"
             document.getElementById("email_signup").classList.remove("is-invalid")
-            document.getElementById("email-feedback_signup").innerHTML = ""
-            const users = JSON.parse(localStorage.getItem("users"))
-
-            users.push(accountNew)
-
-            localStorage.setItem("users",JSON.stringify([]));
-            
-            console.log(users)
-            // window.location.href = "signin.html"
+            document.getElementById("email-feedback_signup").innerHTML=""
         }
         
         else{
-            document.getElementById("tel").classList.add("is-invalid")
-            document.getElementById("tel-feedback").innerHTML="Số điện thoại hoặc email đã được đăng kí"
-            document.getElementById("email_signup").classList.add("is-invalid")
-            document.getElementById("email-feedback_signup").innerHTML="Email hoặc số điện thoại đã được đăng kí"
+            users.push(user);
+            localStorage.setItem("users", JSON.stringify(users));
+            console.log(users)
+            document.getElementById("email_signup").classList.remove("is-invalid")
+            document.getElementById("email-feedback_signup").innerHTML=""
+            document.getElementById("tel").classList.remove("is-invalid")
+            document.getElementById("tel-feedback").innerHTML=""
         }
     }
     else{
-        console.log("đã nhảy vào hàm else1")
-        if (input_phone == ""){
-            document.getElementById("tel").classList.add("is-invalid")
-            document.getElementById("tel-feedback").innerHTML = "Không được để trống số điện thoại"
-        }
-        else{
-            document.getElementById("tel").classList.remove("is-invalid")
-            document.getElementById("tel-feedback").innerHTML = ""
-        }
-        if (input_email_signup == ""){
-            document.getElementById("email_signup").classList.add("is-invalid")
-            document.getElementById("email-feedback_signup").innerHTML="Không được để trống email"
-        }
-        else{
-            document.getElementById("email_signup").classList.remove("is-invalid")
-            document.getElementById("email-feedback_signup").innerHTML = ""
-        }
-        
-        if (!input_password){
-            document.getElementById("password_signup").classList.add("is-invalid")
-            document.getElementById("pass-feedback_signup").innerHTML="Không được để trống password"
-        }
-        else{
-            document.getElementById("password_signup").classList.remove("is-invalid")
-            document.getElementById("pass-feedback_signup").innerHTML = ""
-        }
-        
-        if (input_cf_password == ""){
-            document.getElementById("cf_password").classList.add("is-invalid")
-            document.getElementById("cf_pass-feedback").innerHTML="Vui lòng xác nhận password"
-        }
-        else{
-            document.getElementById("cf_password").classList.remove("is-invalid")
-            document.getElementById("cf_pass-feedback").innerHTML = ""
-        }
-        
-        if (input_cf_password != input_password){
-            document.getElementById("cf_password").classList.add("is-invalid")
-            document.getElementById("cf_pass-feedback").innerHTML="Không trùng khớp password"
-        }
-        else{
-            document.getElementById("cf_password").classList.remove("is-invalid")
-            document.getElementById("cf_pass-feedback").innerHTML=""
-        }
+        console.log("có lỗi")
     }
+    
+
+    
 })
 
 
